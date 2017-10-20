e9c77f7a959e11c01f57a0b195634e6b9567c959
majorbonghits420 <jcc384@cornell.edu>
2017-10-19 21:55:45 -0400

    Add class for SHA256 hashes

---
+++ src/main/java/utils/Sha256.java
@@ -0,0 +1,69 @@
+package utils;
+
+import network.Data;
+
+import java.io.ByteArrayOutputStream;
+import java.lang.RuntimeException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+/**
+ * Class that represents a SHA256 hash.
+ * Also includes utils for generating hashes
+ */
+public class Sha256 implements Data{
+    public static final int HASH_SIZE = 32;
+
+    private byte[] hash;
+
+    /**
+     * Creates a SHA256 from the given hash.
+     * If the given hash is larger than 32 bytes, the hash is truncated to 32 bytes.
+     *
+     * @param hash A byte array containing a SHA256 hash.
+     */
+    Sha256(byte[] hash) {
+        this.hash = Arrays.copyOf(hash, HASH_SIZE);
+    }
+
+    public byte[] getByteArray() {
+        return Arrays.copyOf(hash, HASH_SIZE);
+    }
+
+    public double getByteSize() {
+        return HASH_SIZE;
+    }
+
+    public static Sha256 getHash(byte[] toHash) {
+        try {
+            MessageDigest msgDigest = MessageDigest.getInstance("SHA-256");
+            msgDigest.update(toHash);
+            return new Sha256(msgDigest.digest());
+        } catch (NoSuchAlgorithmException e) {
+            // Should never happen, algo is hard coded...
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Sha256 getHash(ByteArrayOutputStream out) {
+        return getHash(out.toByteArray());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        } else if( obj == null || !(obj instanceof Sha256)) {
+            return false;
+        }
+        Sha256 o = (Sha256) obj;
+        return Arrays.equals(hash, o.hash);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(hash);
+
+    }
+}
