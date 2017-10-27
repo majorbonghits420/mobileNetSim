9f61c8e39d04270fcd66c0ab5d4f538268973b17
majorbonghits420 <jcc384@cornell.edu>
2017-10-27 01:05:10 -0400

    Add the Blockchain object

---
+++ src/main/java/blockchain/Blockchain.java
@@ -0,0 +1,72 @@
+package blockchain;
+
+import utils.Sha256;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class Blockchain {
+
+    HashMap<Sha256, Block> chain;
+
+    Blockchain(List<Block> blocks) {
+        chain = new HashMap<Sha256, Block>();
+        this.addBlocks(blocks);
+    }
+
+    Blockchain(Block b) {
+        this(Arrays.asList(b));
+    }
+
+
+    public void addBlock(Block b) {
+        addBlocks(Arrays.asList(b));
+    }
+
+    public void addBlocks(List<Block> blocks) {
+        for (Block b : blocks) {
+            chain.put(b.getHash(), b);
+        }
+    }
+
+    public List<Sha256> getFrontierHashes() {
+        // Collect all the parents
+        ArrayList<Sha256> parentHashes = new ArrayList<Sha256>();
+        for (Map.Entry<Sha256, Block> kv : chain.entrySet()) {
+            parentHashes.add(kv.getValue().getParent());
+        }
+
+        // Check if each block is in the set of parents. If it isn't its a frontier node
+        ArrayList<Sha256> frontiers = new ArrayList<Sha256>();
+        for (Block b : chain.values()) {
+            Sha256 hash = b.getHash();
+            if (!(parentHashes.contains(hash))) {
+                frontiers.add(hash);
+            }
+        }
+        return frontiers;
+    }
+
+    public Block getBlock(Sha256 hash) {
+        return chain.get(hash);
+    }
+
+    /**
+     * Returns true is the given block is in the chain, false else
+     */
+    public boolean containsBlock(Block b) {
+        return containsBlock(b.getHash());
+    }
+
+    /**
+     * Checks the chain for the block corresponding to the given hash
+     */
+    public boolean containsBlock(Sha256 hash) {
+        return chain.containsKey(hash);
+    }
+
+}
