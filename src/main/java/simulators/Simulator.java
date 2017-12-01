f89dbcc2183b36f7b534f93ed0c07c810ccce74b
majorbonghits420 <jcc384@cornell.edu>
2017-12-01 01:31:33 -0500

    Add Doxygen documentation to Simulator.java

--- src/main/java/simulators/Simulator.java
+++ src/main/java/simulators/Simulator.java
@@ -1,46 +1,81 @@
+package simulators;
+
 import node.Node;
 
 import java.lang.Math;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Simulator {
-    private ArrayList<Node> nodes;
-    private double timeStep; /** The timestep of the simulation in seconds. */
+/**
+ * Abstract class that defines what all simulations will have. Each simulation will have a timestep
+ * and a set of nodes associated with it which will be simulated. The Simulator object will be in
+ * charge of simulating each node, creating channels between nodes, and updating any other state
+ * that is specific to the given simulation.
+ */
+public abstract class Simulator {
+    private ArrayList<Node> nodes; /**< A list of all the nodes contained in the simulation */
+    private double timeStep; /**< The timestep of the simulation in seconds. */
 
-    Simulator(double timeStep) {
+    /**
+     * Constructs a simulator object with a given timestep and no associated Nodes.
+     *
+     * @param timeStep The timestep in seconds of the simulation
+     */
+    protected Simulator(double timeStep) {
         this(timeStep, new ArrayList<Node>());
     }
 
-    Simulator(double timeStep, List<Node> nodes) {
+    /**
+     * Constructs a simulator object with a given timestep and list of accosciated nodes/
+     *
+     * @param timeStep The timestep in seconds of the simulation
+     * @param nodes A List of Nodes that are to be simulated
+     */
+    protected Simulator(double timeStep, List<Node> nodes) {
         this.nodes = new ArrayList<Node>(nodes);
         this.timeStep = timeStep;
     }
 
     /**
-     * Initializes the simulator
-     */
-    public void init() {}
-
-    /**
      * Performs the simulation for a given duration of time.
      *
+     * @param duration A double describing the amount of time to run the simulation in seconds
      */
     public abstract void run(double duration);
 
     /* Getters and setters */
+    /**
+     * Returns the timestep of the simulation
+     *
+     * @return double representing the timestep in seconds
+     */
     public double getTimeStep() {
         return timeStep;
     }
 
+    /**
+     * Sets the timestep for the simulation
+     *
+     * @param t A double representing the new timestep for the simulation in seconds
+     */
     public void setTimeStep(double t) {
         timeStep = t;
     }
 
+    /**
+     * Adds a node to the list of nodes to be included in the simulation
+     *
+     * @param n A Node object to add to be simulated by this object
+     */
     public void addNode(Node n) {
         nodes.add(n);
     }
 
+    /**
+     * Returns the list of nodes simulated by this Simulator object
+     *
+     * @param A list of Nodes
+     */
     public List<Node> getNodes() {
         return nodes;
     }
