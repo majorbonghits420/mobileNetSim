7c765e4f7a32a8017185d6fc6c7f38abdcf12cf6
majorbonghits420 <jcc384@cornell.edu>
2017-12-13 08:04:12 -0500

    Add graph for second simulation

--- genGraphs.m
+++ genGraphs.m
@@ -22,7 +22,7 @@ plot(xaxis, rangeTime(st, 3:end), ...
      xaxis, rangeTime(st + 5, 3:end));
 axis([0 160 0 500]);
 xlabel("Number block spread through network");
-ylabel("Time to Spread to 100% of Nodes");
+ylabel("Time to Spread to 100% of Nodes (seconds)");
 title("Spread Time of Blocks to all Nodes based on Select Ranges");
 legend("10m", "20m", "30m", "40m", "50m");
 
@@ -34,20 +34,28 @@ plot(xaxis, rangeTime(st + 6, 3:end), ...
      xaxis, rangeTime(st + 10, 3:end));
 axis([0 400 0 500]);
 xlabel("Number block spread through network");
-ylabel("Time to Spread to 100% of Nodes");
+ylabel("Time to Spread to 100% of Nodes (seconds)");
 title("Spread Time of Blocks to all Nodes based on Select Ranges");
 legend("60m", "70m", "80m", "90m", "100,");
+% Find average for 70m
+row2 = rangeTime(st + 7, 3:end);
+rmz = row2(row2 != 0.0);
+mean70 = mean(rmz);
 
 % Make the graphs for the passive nodes
 passive = csvread("SpreadSimPassiveResultsV1.csv");
 percents = [10, 20, 30, 40, 50 ,60 ,70 ,80 ,90];
 st = 4;
-averages = [];
+
 for i = [1 :9]
-  row = passive(st + 1, 2:end);
-  removedZeros = row(row != 0.0 | row > 100.0);
+  row = passive(st + i, 2:end);
+  removedZeros = row(row != 0.0);
   averages(i) = mean(removedZeros);
 end
-
+averages(end) = 3600; 
 figure(4);
-plot(percents, averages);
+plot(percents, averages, percents, [mean70, mean70, mean70, mean70, mean70, mean70, mean70, mean70, mean70]);
+xlabel("Percentage of Nodes that are Passive");
+ylabel("Average Time for a Block to Spread (seconds)");
+title("Avg. Block Spread Time vs. Passive Percentage of Network");
+legend("Passive Node Influce", "Baseline (No Passive Nodes)");
