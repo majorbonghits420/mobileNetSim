c2cbd05f00d143466ee5b2d79f18a9b17e710269
majorbonghits420 <jcc384@cornell.edu>
2017-12-13 04:24:54 -0500

    Add test for passive nodes to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import mobility.StaticPoint;
 import mobility.RandomWalk;
 import network.FastChannel;
 import node.Node;
+import node.PassiveNode;
 import simulators.SpreadSim;
 
 import java.util.ArrayList;
@@ -13,7 +14,44 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        runTrustedNodesRangeTest();
+        passiveImpact();
+    }
+
+    public static void passiveImpact() {
+        double bw = 100000; // 100 KB/s
+        double maxNodeBw = 1000000; // 1 MB/s
+        int numNodes = 50;
+        double range = 70;
+
+        for (double percentPassive = 0.1; percentPassive <= 0.6; percentPassive += 0.1) {
+            // Output the percentage of passive nodes
+            System.out.print(percentPassive);
+            ArrayList<Node> nodes = new ArrayList<Node>();
+            int numPass = (int) (percentPassive * numNodes);
+            for (int i = 0; i < numNodes; i++) {
+                RandomWalk m = new RandomWalk();
+                for (int t = 0; t < 120; t++) {
+                    m.model(1.0);
+                }
+                // Create a passive node
+                if (i < numPass) {
+                    PassiveNode p = new PassiveNode(m, maxNodeBw, range);
+                    // Create an active node
+                } else {
+
+                    Node n = new Node(m, maxNodeBw, range);
+                    nodes.add(n);
+                }
+            }
+
+            // Create our simulation
+            double simStep = 0.5; // 0.5 seconds
+            SpreadSim sim = new SpreadSim(simStep, nodes, bw);
+
+            // Run the simulation
+            sim.run(3600); // Run the simulation for 1 hour
+            System.out.println();
+        }
     }
 
     public static void runTrustedNodesRangeTest() {
