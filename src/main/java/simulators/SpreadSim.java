c0081d73da0b8bcfd86abd11a8452c02ca27a36c
majorbonghits420 <jcc384@cornell.edu>
2017-12-13 01:09:43 -0500

    Minor updates to SpreadSim concerning generation of Blocks

--- src/main/java/simulators/SpreadSim.java
+++ src/main/java/simulators/SpreadSim.java
@@ -8,13 +8,23 @@ import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.List;
 
+/**
+ * Simulates and times the Spread of a single Block in a network of nodes
+ */
 public class SpreadSim extends Simulator {
 
     private Block toCheckFor; /**< The current Block we are checking every node gets */
     private double channelBw; /**< Bandwidth of created channels */
-    private double currentTime;
-    private double blockGenTime;
+    private double currentTime; /**< Current time in the simulation */
+    private double blockGenTime; /**< Time when the Block being distributed was generated */
 
+    /**
+     * Creates a SpreadSim with the given nodes, timestep, and bandwidth
+     *
+     * @param timestep The timestep the simulation should take in seconds
+     * @param nodes Nodes to simulate
+     * @param bandwidth The max bandwidth of created channels between nodes
+     */
     public SpreadSim(double timestep, List<Node> nodes, double bandwidth) {
         super(timestep, nodes);
         // Set to Genisis as default
@@ -24,6 +34,13 @@ public class SpreadSim extends Simulator {
         currentTime = 0.0;
     }
 
+    /**
+     * Creates a SpreadSim with the given timestep, and bandwidth.
+     * Nodes must be added later for simulation to be useful
+     *
+     * @param timestep The timestep the simulation should take in seconds
+     * @param nodes Nodes to simulate
+     */
     public SpreadSim(double timestep, double bandwidth) {
         this(timestep, new ArrayList<Node>(), bandwidth);
     }
@@ -37,6 +54,14 @@ public class SpreadSim extends Simulator {
         blockGenTime = currentTime;
     }
 
+    /**
+     * Runs the simulation for a given duration.
+     * Simulates a block being distributed to all nodes, once this occurs, it randomly chooses a node to generate a block.
+     * It then waits for this block to spread throughout the network. This is repeated until the duration is hit. When
+     * a block makes it to all blocks the amount of time it took is outputted to STDOUT
+     *
+     * @param duration How much simulated time should occur in seconds
+     */
     public void run(double duration) {
         do {
             currentTime += timeStep;
@@ -60,10 +85,9 @@ public class SpreadSim extends Simulator {
             // If all Nodes have the Block, pick a random node to generate a new one
             if (allNodesContain(toCheckFor)) {
                 // Output how long it took to spread the Block, update vars
-                System.out.println((currentTime - blockGenTime));
+                System.out.print((currentTime - blockGenTime) + ", ");
 
-                int randNodeIndex = ThreadLocalRandom.current().nextInt(0, nodes.size());
-                Block newBlock = nodes.get(randNodeIndex).generateBlock();
+                Block newBlock = generateRandomBlock();
                 toCheckFor = newBlock;
                 blockGenTime = currentTime;
             }
@@ -87,6 +111,25 @@ public class SpreadSim extends Simulator {
         return (recvd / nodes.size()) * 100.0;
     }
 
+    /**
+     * Selected a random node in the simulation to randomly generate a Block.
+     *
+     * @return The generated block from the randomly selected node
+     */
+    protected Block generateRandomBlock() {
+        Block newBlock = null;
+        while (newBlock == null) {
+            int randNodeIndex = ThreadLocalRandom.current().nextInt(0, nodes.size());
+            newBlock = nodes.get(randNodeIndex).generateBlock();
+        }
+        return newBlock;
+    }
+    /**
+     * Checks if all nodes in the simulation contatin a given Block
+     *
+     * @param b The block to check each node for
+     * @return true is every block contains the given Block, false otherwise
+     */
     private boolean allNodesContain(Block b) {
         boolean spread = true;
         for (Node n : nodes) {
